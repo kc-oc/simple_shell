0e0572198eaa1c7fbab2b5ba3e20d73fa81997d3
Idodoo <dodoo.ivan@yahoo.com>
2023-05-25 17:50:53 +0000

    fixing errrs

--- k_atoi.c
+++ k_atoi.c
@@ -54,16 +54,28 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-	int i;
+	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
-	
-	for (i = 0; s[i] != '\0'; i++)
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
+		if (s[i] == '-')
+			sign *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + (s[i] - '0');
-		else
-			return (-1);
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
 
-	return (result);
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
